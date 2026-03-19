47fb9e17721797c0e1f6401f55ad69885127cd05
sstbi <ernani@sstbi.com.br>
2026-03-18 23:22:59 -0300

    feat: adiciona mensagens de sucesso e confirmação de inativação
    Implementado feedback visual ao usuário após operações de cadastro, edição e inativação de empresas.
    
    Adicionada confirmação antes da inativação para evitar ações indevidas.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app import db
 from app.models import Empresa
 
+# teste commit
+
 main = Blueprint("main", __name__)
 
 @main.route("/")
